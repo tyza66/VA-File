b11e742a8e7d974837a6d7df9021100e088d0be3
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-07-22 12:04:48 +0800

    更新

---
+++ backend/java/VFM_Core/src/main/java/com/tyza66/vfm_core/VfmCoreApplication.java
@@ -0,0 +1,13 @@
+package com.tyza66.vfm_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfmCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VfmCoreApplication.class, args);
+    }
+
+}
---
+++ backend/java/VFM_Core/src/test/java/com/tyza66/vfm_core/VfmCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyza66.vfm_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfmCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
