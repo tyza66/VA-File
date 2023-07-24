198434df05f9c77144a629f45e827ee668a0414c
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-07-24 21:05:09 +0800

    更新了一些搜索的界面

--- frontend/vue/src/views/SearchView.vue
+++ frontend/vue/src/views/SearchView.vue
@@ -27,9 +27,36 @@
       <el-button class="rb" type="default" size="small">设置</el-button>
       <el-button class="rb" type="default" size="small">返回主页</el-button>
     </div>
-    <div>
+    <div class="items">
       <el-row class="item-one">
-        <el-col  :span="1"></el-col>
+        <el-col class="icon" :span="1">
+          图标
+        </el-col>
+        <el-col class="type" :span="2">
+          类型
+        </el-col>
+        <el-col class="name" :span="5">
+          名称
+        </el-col>
+        <el-col class="size" :span="3">
+          大小
+        </el-col>
+        <el-col class="path" :span="5">
+          路径
+        </el-col>
+        <el-col class="path" :span="8">
+          操作
+        </el-col>
+      </el-row>
+      <el-row class="item-one">
+        <el-col class="icon" :span="1">
+          <svg viewBox="0 0 1024 1024" version="1.1"
+            xmlns="http://www.w3.org/2000/svg" p-id="2324" width="30" height="30">
+            <path
+              d="M855.04 385.024q19.456 2.048 38.912 10.24t33.792 23.04 21.504 37.376 2.048 54.272q-2.048 8.192-8.192 40.448t-14.336 74.24-18.432 86.528-19.456 76.288q-5.12 18.432-14.848 37.888t-25.088 35.328-36.864 26.112-51.2 10.24l-567.296 0q-21.504 0-44.544-9.216t-42.496-26.112-31.744-40.96-12.288-53.76l0-439.296q0-62.464 33.792-97.792t95.232-35.328l503.808 0q22.528 0 46.592 8.704t43.52 24.064 31.744 35.84 12.288 44.032l0 11.264-53.248 0q-40.96 0-95.744-0.512t-116.736-0.512-115.712-0.512-92.672-0.512l-47.104 0q-26.624 0-41.472 16.896t-23.04 44.544q-8.192 29.696-18.432 62.976t-18.432 61.952q-10.24 33.792-20.48 65.536-2.048 8.192-2.048 13.312 0 17.408 11.776 29.184t29.184 11.776q31.744 0 43.008-39.936l54.272-198.656q133.12 1.024 243.712 1.024l286.72 0z"
+              p-id="2325"></path>
+          </svg>
+        </el-col>
       </el-row>
     </div>
   </div>
@@ -76,6 +103,11 @@
   margin-right: 10px;
 }
 
+.items{
+  display: flex;
+  flex-direction: column;
+}
+
 .item-one{
   margin-top: 10px;
   margin-left: 10px;
@@ -93,6 +125,23 @@
 
 .item-one el-col{
   height: 36px;
+}
+
+.item-one .icon,.item-one .type,.item-one .name,.item-one .size,.item-one .path{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.item-one .icon svg{
+  height: 30px;
+  widows: 30px;
+}
+
+.item-one .name{
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
 
